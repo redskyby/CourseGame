0e545c34acf924b4672d4935c8f7625f816f34db
redskyby <pashadocenko@gmail.com>
2021-11-10 21:01:45 +0300

    Add project files.

---
+++ CourseGame/CourseGame.cpp
@@ -0,0 +1,223 @@
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace sf;
+
+//класс для спрайтов
+class Object {
+public:
+    Sprite fon, game, exit , block[18] , player , ball;
+
+    RectangleShape line;
+};
+
+//Menu
+class Menu {
+public:
+    bool menu = true;
+    bool game = false;
+ };
+
+
+
+int main()
+{
+    double kx = 0;
+    double ky = 0;
+    double SpeedBoard = 0.2;
+    double SpeedBall = 0.09;
+
+    int eventGameStart = 0;
+
+    //создание окна отрисовки
+    RenderWindow window(VideoMode(1200, 800), "Arcanoid");
+
+    Object obj;
+    Menu men;
+
+    //Фон
+    Texture t1, t2, t3, t4, t5, t6;
+    t1.loadFromFile("Paint/fon.png");
+    obj.fon.setTexture(t1);
+
+    //Кнопка играть
+    t2.loadFromFile("Paint/nac.png");
+    obj.game.setTexture(t2);
+    obj.game.setPosition(80, 400);
+
+    //Кнопка выхода
+    t3.loadFromFile("Paint/exit.png");
+    obj.exit.setTexture(t3);
+    //Кнопка выхода -> изменение маштаба
+    obj.exit.scale(0.7f, 0.7f);
+    obj.exit.setPosition(800, 400);
+
+    //Блоки
+    t4.loadFromFile("Paint/block.png");
+    //Цвет блока
+    for (int i = 0; i < 6; i++) {
+        for (int j = 0; j < 3; j++) {
+            obj.block[j * 6 + i].setTexture(t4);
+            obj.block[j * 6 + i].setTextureRect(IntRect(0, 0, 140, 26));
+        }
+    }
+
+    //Верхняя линия
+    obj.line.setSize(Vector2f(1200, 1));
+    obj.line.setFillColor(Color::White);
+    obj.line.setPosition(0, 100);
+
+    //Игрок
+    t5.loadFromFile("Paint/player.png");
+    obj.player.setTexture(t5);
+
+    //Мяч
+    t6.loadFromFile("Paint/ball.png");
+    obj.ball.setTexture(t6);
+
+   
+   
+
+    while (window.isOpen()) {
+        Event event;
+
+        while (window.pollEvent(event)) {
+            if (event.type == Event::Closed) {
+                window.close();
+            }
+        }
+            //События на кнопки меню
+            if (event.type == Event::MouseButtonPressed) {
+                if (event.key.code == Mouse::Left) {
+                    Vector2i pos = Mouse::getPosition(window);
+
+                    if (men.menu) {
+                        //Событие на кнопку выхода
+                        if (pos.x >= 800 && pos.x <= 1000 &&
+                            pos.y >= 410 && pos.y <= 520) {
+                            window.close();
+                        }
+                    }
+
+                    //Событие на кнопку играть
+                    if (pos.x >= 50 && pos.x <= 380 &&
+                        pos.y >= 400 && pos.y <= 500) {
+                        men.menu = false;
+                        men.game = true;
+
+                        for (int i = 0; i < 6; i++) {
+                            for (int j = 0; j < 3; j++) {
+                                obj.block[j * 6 + i].setPosition(25 + 200 * i, 100 + 100 * (j + 1));
+                            }
+                        }
+                        //Координаты игрока и мяча
+                        obj.player.setPosition(400, 750);
+                        obj.ball.setPosition(510, 660);
+                        kx = 1;
+                        ky = 1;
+
+                    }
+
+                }
+            }
+
+            //Если игра == тру , то откликаться на клавиши
+            Vector2f BoardEvent = obj.player.getPosition();
+            if (men.game) {
+                //Правая D
+                if (Keyboard::isKeyPressed(Keyboard::D)) {
+                    if (BoardEvent.x < 870) {
+                        obj.player.move(SpeedBoard, 0);
+                        if (eventGameStart == 0 ) {
+                            obj.ball.move(SpeedBoard, 0);
+                        }
+                    }
+                }
+
+                //Левая A
+                if (Keyboard::isKeyPressed(Keyboard::A)) {
+                    if (BoardEvent.x > 0) {
+                        obj.player.move(-SpeedBoard, 0);
+                        if (eventGameStart == 0) {
+                            obj.ball.move(-SpeedBoard, 0);
+                        }
+                    }
+                }
+
+               //Начало игры с нажатие пробела 
+                if (Keyboard::isKeyPressed(Keyboard::Space)) {
+                    eventGameStart = 1;
+                }
+            }
+
+            //Если игра началась
+            if (eventGameStart) {
+                obj.ball.move(SpeedBall * -kx, SpeedBall * -ky);
+
+                Vector2f PlayerInGame = obj.player.getPosition();
+                Vector2f BallInGame = obj.ball.getPosition();
+                //Отскакивание от стен
+                if ((BallInGame.y < 0) || (BallInGame.y < 100)) {
+                    ky *= -1;
+                }
+                if (BallInGame.x > 1110 || BallInGame.x < 0) {
+                    kx *= -1;
+                }
+
+                //Игрок отбивает мяч
+                if (obj.ball.getGlobalBounds().intersects(obj.player.getGlobalBounds())) {
+                    ky *= -1;
+
+                    obj.ball.move(SpeedBall, -0.5);
+                }
+                //Если мячик упал
+                if (BallInGame.y > 800) {
+                    obj.player.setPosition(400, 750);
+                    obj.ball.setPosition(510, 660);
+                    eventGameStart = 0;
+                }
+
+
+                
+                for (int i = 0; i < 18; i++) {
+                    if (obj.ball.getGlobalBounds().intersects(obj.block[i].getGlobalBounds())) {
+                        ky *= -1;
+                        obj.ball.move(SpeedBall, 0.1 * ky);
+
+                        obj.block[i].setPosition(1300, 0);
+                    }
+                }
+            }
+        
+
+
+
+
+
+
+        window.draw(obj.fon);
+
+        //Если меню true , то видим меню
+        if (men.menu) {
+            window.draw(obj.game);
+            window.draw(obj.exit);
+        }
+        //Если нажата кнопка "Играть"  , то играем
+        if (men.game) {
+            window.draw(obj.line);
+            for (int i = 0; i < 18; i++) {
+                window.draw(obj.block[i]);
+            }
+            window.draw(obj.ball);
+            window.draw(obj.player);
+        }
+
+        window.display();
+
+    }
+ 
+  
+
+    return 0;
+}
